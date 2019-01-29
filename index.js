7ae53375cf12e9933a520d2506c7bb77ce2e39be
xjp <331000738@qq.com>
2019-01-29 12:10:20 +0800

    2019年 1月29日 星期二 12时10分20秒 CST

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const css = require('sheetify')
 const choo = require('choo')
 const root = require('window-or-global')
 // const async = require('choo-async') //es6
+const reload = require('choo-reload')
+
 
 css('tachyons')
 css('./node_modules/bootstrap/dist/css/bootstrap.min.css')
 
+
 const app = choo()
+
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')())
 } else {
   app.use(require('choo-service-worker')())
 }
+
 
 app.use((state, emitter) => {
   state.global_attr1 = 'global_attr1' //全局状态
@@ -21,8 +26,12 @@ app.use((state, emitter) => {
 })
 root.emitter = app.emitter //@todo: 各页面自己require
 
+app.use(reload())
+
+
 app.route('/', require('./views/home'))
 app.route('/about', require('./views/about'))
 app.route('/*', require('./views/404'))
+
 
 module.exports = app.mount('body')

b7e98746a290a96b505943960d4270dc245e5b59
xjppad <xiaojp@hztywl.cn>
2019-02-11 23:03:02 +0800

    wish.js

--- pages/wish.js
+++ pages/wish.js
@@ -6,17 +6,48 @@ const html = require('choo/html')
 const TabBar = require('../components/TabBar')
 const NavBar = require('../components/NavBar')
 
+const pageState = {
+  wishes: [
+    {name: '看电视40分钟', cost: 2, },
+    {name: '买零食一件', cost: 4, },
+    {name: '买玩具一件', cost: 50, },
+  ]
+}
+
+function _WishItem (wish){
+  return html`
+    <div class="flex justify-between pv2 bb b--black-10">
+      <span class="f6"> ${wish.name}</span>
+      <span class="gray">所需金币:${wish.cost}</span>
+    </div>
+  `
+}
+
+function WishList(){
+  return html`
+    <section class="mh1 mb2">
+      ${pageState.wishes.map(wish => _WishItem(wish))}
+    </section>
+  `
+}
+
+function AddWishBtn () {
+  return html`
+    <button class="van-button van-button--primary van-button--large">
+      <span class=van-button__text>添加心愿</span>
+    </button>
+  `
+}
+
 function Main () {
   emitter.emit('DOMTitleChange', '心愿')
 
-  return html/*syntax:html*/`
+  return html`
     <body>
       ${NavBar({title:'心愿'})}
       <main class="pa1 cf center" style="margin-bottom:50px">
-        <p>测试文字1</p>
-        <p>测试文字2</p>
-        <p>测试文字3</p>
-        <a href="/">[回首页]</a>
+        ${WishList()}
+        ${AddWishBtn()}
       </main>
       ${TabBar({ currentTab: 'wish'})}
     </body>
